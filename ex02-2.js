331bbcc0bed784c1bcce63f0bb1fdfdabb4d2a72
menoncut <133994207+menoncut@users.noreply.github.com>
2024-04-08 06:58:15 +0900

    Nodejs
    Nodejs Practice

---
+++ ex02-2.js
@@ -0,0 +1,7 @@
+console.log("name:%s", "Hong Gil Dong");
+console.log("age:%d", 20);
+console.log("pi:%d", 3.14);
+console.log("math:%d science:%d", 95, 99);
+console.log("name:%s age:%d", "Hong Gil Dong", 20);
+console.log("name:%s math:%d science:%d", "Hong Gil Dong", 95, 99);
+console.log("name:%s math:%s science:%s", "Hong Gil Dong", 95, 99);
---
+++ ex02-4.js
@@ -0,0 +1,15 @@
+// 한줄 주석 처리하기
+
+/*
+여러 줄 주석 처리하기
+*/
+
+console.log("hello");
+//console.log('bye');
+
+console.log("welcome");
+/*
+console.log('Have');
+console.log('a nice');
+console.log('day');
+*/
